fc929c56f82e6683e6f28199898eb138c7d6925b
Star <star3s@126.com>
2024-09-19 15:38:03 +0800

    update exit signal

--- gowatch/gowatch.go
+++ gowatch/gowatch.go
@@ -184,16 +184,17 @@ func main() {
 	run()
 
 	if w, err := watcher.Start(basePaths, watchTypes, watchCallback); err == nil {
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
+		exitCh := make(chan os.Signal, 1)
+		closeCh := make(chan bool, 1)
+		signal.Notify(exitCh, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
 		go func() {
-			<-c
+			<-exitCh
+			w.Stop()
 			stop()
 			fmt.Println("\nExit")
-			os.Exit(0)
+			closeCh <- true
 		}()
-		<-c
-		w.Stop()
+		<-closeCh
 	} else {
 		fmt.Println(err.Error())
 	}
